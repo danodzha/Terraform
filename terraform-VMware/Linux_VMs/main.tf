92ce55eb753ae38ca180da43c53c2db4959c0875
danodzha <daniiar.valiev@gmail.com>
2020-02-27 02:28:09 +0000

    restructured folders and files

--- terraform-VMware/Linux_VMs/main.tf
+++ terraform-VMware/Linux_VMs/main.tf
@@ -1,6 +1,33 @@
-#Resources
+provider "vsphere" {
+  user           = "${var.vcenter_user}"
+  password       = "${var.vcenter_password}"
+  vsphere_server = "${var.vcenter_name}"
+
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "${var.datacenter_name}"
+}
+data "vsphere_datastore" "datastore" {
+  name          = "${var.datastore_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_resource_pool" "pool" {
+  name          = "${var.resources_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_network" "network" {
+  name          = "${var.network_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+data "vsphere_virtual_machine" "template" {
+  name          = "${var.template_name}"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
 resource "vsphere_virtual_machine" "vm" {
-  count 	   = "${var.vm_count}"
+  count 	         = "${var.vm_count}"
   name             = "${var.vm_name}${count.index}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
@@ -29,7 +56,7 @@ resource "vsphere_virtual_machine" "vm" {
  
       network_interface {
         ipv4_address = "${cidrhost(var.ipaddr, var.start_ip + count.index)}" 
-	ipv4_netmask = "${var.netmask}"
+	      ipv4_netmask = "${var.netmask}"
       }
       ipv4_gateway = "${var.gateway}"
     }
--- terraform-VMware/Linux_VMs/module.tf
+++
@@ -1,3 +0,0 @@
-module "tfvars_file" {
-  source = "./vars-test/"
-}

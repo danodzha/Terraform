902b23970ef30a748a040a9dbcf875c411eabd4f
danodzha <daniiar.valiev@gmail.com>
2020-03-07 03:28:20 +0000

    aligned formatting

--- terraform-VMware/VM_linux_clone.tf
+++ terraform-VMware/VM_linux_clone.tf
@@ -1,5 +1,5 @@
 resource "vsphere_virtual_machine" "vm" {
-  count 	   = "${var.vm_count}"
+  count            = "${var.vm_count}"
   name             = "${var.vm_name}${count.index}"
   resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
   datastore_id     = "${data.vsphere_datastore.datastore.id}"
@@ -19,16 +19,16 @@ resource "vsphere_virtual_machine" "vm" {
 
   clone {
     template_uuid = "${data.vsphere_virtual_machine.template.id}"
- 
+
     customize {
       linux_options {
-	      host_name = "${var.vm_name}${count.index}"
-	      domain = "${var.vm_name}${count.index}"
+        host_name = "${var.vm_name}${count.index}"
+        domain    = "${var.vm_name}${count.index}"
       }
- 
+
       network_interface {
-        ipv4_address = "${cidrhost(var.ipaddr, var.start_ip + count.index)}" 
-	      ipv4_netmask = "${var.netmask}"
+        ipv4_address = "${cidrhost(var.ipaddr, var.start_ip + count.index)}"
+        ipv4_netmask = "${var.netmask}"
       }
       ipv4_gateway = "${var.gateway}"
     }
